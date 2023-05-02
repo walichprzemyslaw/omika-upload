3ef6064c200be0a96c5a4c384adddcd9f5a69639
Przemysław Walich <walich1995@gmail.com>
2023-05-02 16:36:56 +0200

    initial commit

--- api/models/Employee.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const EmployeeSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  phone: { type: String, required: true },
-});
-
-export default mongoose.model("Employee", EmployeeSchema);
